b074b8d61652ecbec3ae7de4957c072a8ccf5805
Skander <sk_chi@hotmail.com>
2022-02-25 11:36:47 +0100

    nextjs & mui5 template

---
+++ pages/index.jsx
@@ -0,0 +1,62 @@
+import Container from '@mui/material/Container';
+
+const Homepage = () => {
+  return (
+    <Container maxWidth="sm">
+      <h1>Home Page</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore dicta
+        qui nisi ab accusamus voluptates explicabo quod eligendi ipsum corporis
+        architecto recusandae repellendus sit ratione, cumque illo officia rerum
+        nemo. Deleniti dolore nemo quidem, sapiente sit debitis aspernatur est
+        culpa dignissimos facere eveniet iure ratione nisi repellat ex aut,
+        harum quasi maxime vitae? Ab dolor quod blanditiis ratione, accusamus
+        asperiores! Repellendus maxime quam, iste consequatur earum tenetur
+        accusamus quaerat, accusantium sapiente perferendis ratione? Quidem
+        deleniti nostrum, dolores minima distinctio quisquam ad blanditiis minus
+        tenetur ab consectetur aperiam, sapiente incidunt debitis. Deserunt
+        repellat voluptatem cumque sit vero? Ipsa iste quibusdam inventore. Nam
+        provident totam tempore hic distinctio nihil autem, doloremque
+        temporibus facilis aut neque? Perferendis ullam modi non omnis illum
+        necessitatibus. Aspernatur tempore deleniti perspiciatis fuga nulla sunt
+        iste qui quod possimus repellendus vero beatae, enim ad id tempora
+        nostrum inventore deserunt magni saepe excepturi porro accusamus quos
+        ea? Aperiam, beatae! Impedit architecto iusto cum ea porro natus nobis
+        aspernatur, eius quaerat corrupti incidunt et nam laborum consectetur
+        mollitia aperiam enim, voluptate quae quidem, tempora quasi a. Aliquid
+        quas est hic. Ratione esse commodi officiis! Animi in asperiores
+        aspernatur impedit at repellat vero tempora cumque quos nobis alias,
+        porro adipisci dolores suscipit expedita maxime magnam, dolorum esse.
+        Porro explicabo amet ullam. Ab, eos soluta modi aut inventore harum
+        repudiandae facere incidunt commodi dolores animi ad saepe fugit vitae
+        mollitia ipsam quae maiores ut earum iste voluptatibus voluptates fuga
+        voluptatum! Adipisci, obcaecati! Similique facere quaerat nisi, totam
+        harum deleniti at illo consequuntur, sit ad ipsam asperiores dignissimos
+        dolorum veniam corrupti sunt officia exercitationem. Animi aut officia
+        facere recusandae ipsa in doloribus enim. Suscipit, eos. Nostrum, in
+        eaque. Eius dolorem fuga voluptas doloremque saepe nostrum nobis ad
+        architecto cum sint alias incidunt consequuntur vitae quia impedit
+        perspiciatis animi maiores, autem blanditiis! Atque, deserunt. Odit
+        fugiat qui aliquid consequuntur libero eum illum quidem impedit nam
+        omnis vero, quaerat asperiores laboriosam excepturi quae! Ullam ut
+        facilis debitis! Facilis harum sequi qui molestias, laborum fugiat
+        maiores? Vel corporis perspiciatis ullam nostrum soluta officiis
+        doloribus doloremque laudantium quia nam, debitis voluptates iusto harum
+        consectetur ipsa dolorem? Necessitatibus eum architecto eaque inventore
+        sint aliquid obcaecati quo expedita dolores. Iusto, dolore deleniti et
+        quos assumenda quia sunt minima adipisci quasi ex, reiciendis
+        exercitationem non dolorum ipsa repudiandae labore, vero id. Molestias
+        excepturi consequuntur dolores officiis neque laborum facere sint.
+        Aspernatur quas sapiente suscipit quibusdam quasi sit iusto culpa quae
+        odit? Temporibus adipisci iure tempora doloribus. Repellat quod numquam,
+        inventore esse labore suscipit, accusamus ratione voluptatibus magni
+        mollitia in deserunt. Tempora ullam incidunt, expedita odit accusantium
+        maxime, est officiis laudantium modi blanditiis doloribus cum repellat
+        accusamus explicabo qui animi optio quidem iure nesciunt esse id
+        exercitationem aliquid. Eum, cum iste.
+      </p>
+    </Container>
+  );
+};
+
+export default Homepage;
